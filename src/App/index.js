50b514fd0bd40c0019b12763bbe58372048009cd
Kevin Gilpin <kevin.gilpin@gmail.com>
2021-08-16 23:26:12 -0700

    Add Styled Components

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
+import styled from 'styled-components';
+
+const MyButton = styled.button`
+  color: green;
+  &:hover {
+    color: blue;
+    cursor: pointer;
+  }
+`;
+
+const TomatoButton = styled(MyButton)`
+  color: tomato;
+`;
 
 class App extends Component {
   render() {
-    return <div className="App">Hello World!</div>;
+    return (
+      <div className="App">
+        <MyButton>Hello World</MyButton>
+        <TomatoButton>Yessir!</TomatoButton>
+      </div>
+    );
   }
 }
 
