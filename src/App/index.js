e9f07a7c4e99ab34fe8aada6840e58452308f1fe
Kevin Gilpin <kevin.gilpin@gmail.com>
2021-08-17 15:54:53 -0700

    Add AppProvider and hook up to AppBar

--- src/App/index.js
+++ src/App/index.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import AppLayout from './AppLayout';
 import AppBar from './AppBar';
+import { AppProvider } from './AppProvider';
 
 class App extends Component {
   render() {
     return (
       <AppLayout>
-        <AppBar />
-        <h1>Welcome to CryptoDash</h1>
+        <AppProvider>
+          <AppBar />
+          <h1>Welcome to CryptoDash</h1>
+        </AppProvider>
       </AppLayout>
     );
   }
